6c561e1a4402e9eb8b9745d8c209b239482580b5
yotam-biu <nagaryo3@biu.ac.il>
2024-11-23 20:36:15 +0200

    Update orf.py

--- orf.py
+++ orf.py
@@ -1,14 +1,46 @@
-def find_all_starts(dna):
-    pass # Replace the `pass` with your code
+# def find_all_starts(dna):
+#     pass # Replace the `pass` with your code
 
+
+# def find_first_in_register_stop(dna):
+#     pass # Replace the `pass` with your code
+
+
+# def all_orfs_range(dna):
+#     pass # Replace the `pass` with your code
+
+
+# def longest_orf(dna):
+#     pass # Replace the `pass` with your code
+
+
+
+def find_all_starts(dna):
+    starts = []
+    for i in range(len(dna)):
+        if dna[i: i+3] == "ATG":
+            starts.append(i)
+    return starts
 
 def find_first_in_register_stop(dna):
-    pass # Replace the `pass` with your code
-
+    for i in range(0, len(dna), 3):
+        if dna[i:i+3] in ["TGA", "TAG", "TAA"]:
+            return i+3
+    return -1
 
 def all_orfs_range(dna):
-    pass # Replace the `pass` with your code
-
+    starts = find_all_starts(dna)
+    orfs_ranges = []
+    for start in starts:
+        end = find_first_in_register_stop(dna[start:])
+        if end != -1:
+            orfs_ranges.append((start, start+end))
+    return orfs_ranges
 
 def longest_orf(dna):
-    pass # Replace the `pass` with your code
+    longest = ""
+    for orf_range in all_orfs_range(dna.upper()):
+        orf = dna[orf_range[0]: orf_range[1]]
+        if len(orf) > len(longest):
+            longest = orf
+    return longest
