c42f0841b6d3281b8a8b6f6a15aac25f0de690d7
yotam-biu <nagaryo3@biu.ac.il>
2024-11-23 20:18:44 +0200

    Create orf.py

---
+++ orf.py
@@ -0,0 +1,14 @@
+def find_all_starts(dna):
+    pass # Replace the `pass` with your code
+
+
+def find_first_in_register_stop(dna):
+    pass # Replace the `pass` with your code
+
+
+def all_orfs_range(dna):
+    pass # Replace the `pass` with your code
+
+
+def longest_orf(dna):
+    pass # Replace the `pass` with your code
